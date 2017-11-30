acce4e851f7bfdc110699ea922d342d7466feb90
yx179971 <375986529@qq.com>
2017-11-30 11:15:33 +0800

    简单总价计算，支付页面跳转

--- luffy_vue/src/components/buy.vue
+++ luffy_vue/src/components/buy.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <h4>{{temp}}</h4>
-    <ul v-for="item in buyDict">
-      <li></li>
-    </ul>
+    <div v-for="(item,key) in buyDict">
+      <p>{{item.course.img}}</p>
+      <p>{{item.course.name}}</p>
+      <p>{{item.name}}</p>
+      <p>{{item.price}}</p>
+      <div>折后价：<p class="single_price">{{item.price}}</p></div>
+    </div>
+    <div>总价：
+      <div id="total_price">{{total_amount}}</div>
+    </div>
+    <button v-on:click="pay">确认支付</button>
   </div>
 </template>
 
@@ -14,6 +22,7 @@
       return {
         temp: "结算页面",
         buyDict: {},
+        total_amount: 0,
       }
     },
     mounted: function () {
@@ -27,8 +36,32 @@
           method: 'GET',
         })
           .then(function (response) {
-            console.log(response.data)
+            that.buyDict = response.data
+            console.log(that.buyDict)
+            for (let i in that.buyDict) {
+              console.log(that.buyDict[i])
+              that.total_amount += that.buyDict[i]['price']
+            }
+            that.total_amount = that.total_amount.toFixed(2)
           })
+      },
+      pay() {
+        let that = this
+        this.$axios.request({
+          url: that.$store.state.apiList.pay,
+          method: 'POST',
+//          headers: {
+//            'Content-Type': 'multipart/form-data'
+//          },
+//          contentType:'multipart/form-data',
+          data: {
+            subject: '模拟订单1',
+            out_trade_no: '自定义订单号1',
+            money: that.total_amount,
+          }
+        }).then(function (response) {
+          location.href = response.data.url
+        })
       }
     }
   }
