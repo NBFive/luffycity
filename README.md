2adbd387f41f9ea11eaa1d22591b80eea3daa976
lucaq <934840922@qq.com>
2017-11-30 19:30:36 +0800

    commit beifen del gitignore

---
+++ README.md
@@ -0,0 +1,4 @@
+# luffycity
+##  这是一个在线的视频教育平台，有课程可供选择和学习
+## 架构
+### 前端VUE.js 后端Django Restful Framework
--- luffy_vue/src/main.js
+++ luffy_vue/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store/store'
 import axios from 'axios'
+import $ from 'jquery'
 
 
 Vue.config.productionTip = false
