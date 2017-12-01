f077433b7d81ea20fe96ed4f142e86756c4732ee
lucaq <934840922@qq.com>
2017-12-01 19:15:53 +0800

    test

--- luffy/repository/tests.py
+++ luffy/repository/tests.py
@@ -2,16 +2,3 @@ from django.test import TestCase
 
 # Create your tests here.
 
-
-import copy
-d = {'1':1, '2':2}
-print(id(d))
-d2 = copy.deepcopy(d)
-# d2 = d
-print(id(d))
-print(id(d2))
-d2['1']=5
-
-
-
-print(d['1']+d2['1'])
